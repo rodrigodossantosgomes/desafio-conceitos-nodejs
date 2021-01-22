3c5be2b66f6cf8361873bb089919545d9bc48dce
Rodrigo Gomes <dossantos.tb@gmail.com>
2021-01-22 16:42:37 -0300

    Criando Projeto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuidv4, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,16 +10,46 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
-
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie ={ id: uuidv4(), title, url, techs, likes:0 };
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
+
 });
+
+app.get("/repositories", (request, response) => {
+  // TODO
+  return response.json(repositories);
+});
+
+
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
